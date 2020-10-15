48c464965e7a23c8d1f9b52611e26b7eaee963b1
nevooronni <nevooronni@gmail.com>
2020-10-15 03:51:25 +0300

    finished app

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,9 +1,68 @@
-module.exports = function(app, db) {
+const { ObjectID } = require("mongodb");
+
+module.exports = function(app, db) {  
   //when app recieves a post request to the '/notes' path, it will execure the code inside the callback-passing 
   //in a request object(which contains the parameters of JSON of the request) and a response object (used to reply)
-  app.post('/notes', (req, res) => {
-    console.log(req.body)
-    //create not here
-    res.send('Hello')
+
+
+  //create a note
+  app.post('/notes', (req, res) => {    
+    const note = { 
+      text: req.body.body, title: req.body.title 
+    };    
+    //create note
+    db.collection('notes').insert(note, (err, result) => {      
+      if (err) {         
+        res.send({ 'error': err, 'message': 'An error has occurred' });       
+      } else {        
+        res.send(result.ops[0]);      
+      }    
+    });  
+  });
+
+  //fetch a note
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if(err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  //update note 
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id)};
+    const note = { text: req.body.body, title: req.body.title};
+    db.collection('notes').update(details, note, (err, result) => {
+      if(err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send({
+          'message': 'note updated successfully',
+          'update_info': note
+        });
+      }
+    });
+  });
+
+  //delete note
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if(err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send({
+          'message': 'note deleted successfully',
+          'note_id': id
+        });
+      }
+    });
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose'); //use to connect with your db on mongoDB Atlas
 const MongoClient = require('mongodb').MongoClient; //MongoClient interacts with your database
 const bodyParser = require('body-parser');
+const db = require('./config/db'); //connect database instance
 
 //initialize app as instance of express
 const app = express();
@@ -11,6 +13,30 @@ const port = 8000;
 //use body-parser package
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//dont have a db setup yet you are just passing an empty object
-require('./app/routes')(app, {});
-app.listen(port, () => { console.log('We are live on ' + port);});
+//contect to mongoDB Altas cluster
+// mongoose.connect(db.url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+// .then(() => {
+//   console.log("MongoDB Connected…")
+// })
+// .catch(err => console.log(err))
+
+// const database = database.db
+// app.listen(port, () => {    console.log('We are live on ' + port);  });               
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err) 
+  require('./app/routes')(app, database);
+
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+}) 
+
+
+
+
+
+
